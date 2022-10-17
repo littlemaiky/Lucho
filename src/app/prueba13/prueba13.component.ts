8b1171ed58a6eedcc0b23f85b8ef987fcebba413
AbnerZ0 <019201832d@uandina.edu.pe>
2022-10-17 18:02:11 -0500

    primer

--- src/app/prueba13/prueba13.component.ts
+++ src/app/prueba13/prueba13.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Curso } from '../model/curso';
+import { StoreService } from '../services/store.service';
 
 
 @Component({
@@ -13,14 +14,17 @@ export class Prueba13Component implements OnInit {
   seleccionados: string[]=[];
   //seleccionados: number = 0;
 
-  constructor() { }
+  constructor(private storeService: StoreService) {
+    this.seleccionados = storeService.seleccionados;
+   }
 
   ngOnInit(): void {
   }
 
   selecc(id: string): void {
    // this.seleccionados++;
-   this.seleccionados.push(id);
+   //this.seleccionados.push(id);
+   this.storeService.agregarSeleccion(id);
    console.log(this.seleccionados);
   }
 
